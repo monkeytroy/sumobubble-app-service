36cab1a1203ad80397dcc22a8d4146776f89d2a1
Fred Arters <fredarters@gmail.com>
2023-03-22 02:09:50 -0400

    Update vod

--- pages/api/config/[customer].ts
+++ pages/api/config/[customer].ts
@@ -3,10 +3,19 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import Cors from 'cors';
 import { midware } from '@/services/midware';
 import { connectToDb } from '@/services/mongodb';
+import copyright from './copyright.json';
+
+interface Copyrights {
+  [name: string]: string;
+}
+
+const copyrights: Copyrights = copyright;
 
 const cors = Cors({
   methods: ['GET', 'GET', 'HEAD'],
 });
+
+const DEFAULT_TRANSLATION = 'ASV';
 
 type ConfigRes = {
   success: boolean,
@@ -36,43 +45,25 @@ export default async function handler(
 
     if (custConfig) {
 
-      // process funny
-      const configFunny = { ...custConfig.funny };
-      
-      // only going to return one funny item.
-      custConfig.funny = {};
-
-      // priority is the cust config items.  meme, then lines. 
-      if (configFunny.meme) {
-        custConfig.funny = { 
-          meme: configFunny.meme
-        }
-      } else if (configFunny.lines) {
-        custConfig.funny = {
-          lines: configFunny.lines
-        }
-      }
+      const d = new Date();
+      const findToday = parseInt((d.toISOString().split('T')[0]).replace(/\-/g, ''));
 
       // if cust config does not have a funny set.. get daily
-      if ((!custConfig?.funny?.lines || custConfig?.funny?.lines?.length === 0) && !custConfig?.funny?.meme) {
+      if (custConfig?.funny?.enabled && 
+          (!custConfig?.funny?.lines || custConfig?.funny?.lines?.length === 0) && 
+            !custConfig?.funny?.meme) {
         try {
-          const d = new Date();
-          const findToday = parseInt((d.toISOString().split('T')[0]).replace(/\-/g, ''));
           const funnyRes = await db.collection("funny").find({day: { $lte: findToday }}).sort({ day: -1}).limit(1);
 
           if (funnyRes) {
-            const funnyResVals = await funnyRes.toArray();
-            const funnyDaily = funnyResVals[0];
+            const resVals = await funnyRes.toArray();
+            const daily = resVals[0];
             
             // merge in the             
-            if (funnyDaily.meme) {
-              custConfig.funny = {
-                meme: funnyDaily.meme
-              }
-            } else if (funnyDaily.lines) {
-              custConfig.funny = {
-                lines: funnyDaily.lines
-              }
+            if (daily?.meme) {
+              custConfig.funny.meme = daily.meme;
+            } else if (daily?.lines) {
+              custConfig.funny.lines = daily.lines;
             }
           }
         } catch(err) {
@@ -80,7 +71,35 @@ export default async function handler(
         }
       }
 
+      const trans = custConfig.verse.translation || DEFAULT_TRANSLATION;
+      // get translation copyright. 
+      custConfig.verse.copyright = copyrights[trans] || '';
+
+      // if cust config does not have a verse set.. get daily
+      if (custConfig?.verse?.enabled && 
+        (!custConfig?.verse?.verseRef || !custConfig?.verse?.content)) {
+        try {
+          const verseRes = await db.collection("verse").find({day: { $lte: findToday }}).sort({ day: -1}).limit(1);
+
+          if (verseRes) {
+            const resVals = await verseRes.toArray();
+            const daily = resVals[0];
+            
+            // merge in the             
+            custConfig.verse.verseRef = daily.verseRef;
+
+            // find the translation
+            const content: string = daily.verses[trans];
+            custConfig.verse.content = content;
+
+          }
+        } catch(err) {
+          console.log('Could not pull in funny for today.', err);
+        }
+      }
+      
       res.status(200).json({ success: true, message: 'Message for you sir!', data: custConfig });
+
     } else {
       console.log('Could not get config for client: ' + customer);
       res.status(400).send({ success: false, message: 'No config found'});
