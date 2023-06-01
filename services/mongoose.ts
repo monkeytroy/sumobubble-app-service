b48221669d08aaad05c99519850bbe7064803f1e
Fred Arters <fredarters@gmail.com>
2023-06-01 02:22:50 -0400

    Trying to keep db conn count low

--- services/mongoose.ts
+++ services/mongoose.ts
@@ -5,12 +5,18 @@ const MONGO_CONNECT = process.env.MONGO_CONNECT;
 let mongoConnected = false
 
 const connectMongo = async () => {
-  console.log(`Connecting with ${MONGO_CONNECT}`);
+
+  const ready = mongoose?.connection?.readyState;
+  mongoConnected = (ready == 1 || ready == 2);
+
+  console.log(`connectMongo:: currently connected ${mongoConnected} readyState ${ready} connection count ${mongoose?.connections?.length}`);
+  
   if (MONGO_CONNECT && !mongoConnected) {
+    console.log(`Connecting with ${MONGO_CONNECT}`);
     const res = await mongoose.connect(MONGO_CONNECT);
     if (res) {
       mongoConnected = true;
-      console.log(`Connected with ${MONGO_CONNECT}`);
+      console.log(`Connected`, res.connection.readyState);
     } else {
       mongoConnected = false;
       console.log(`Failed to connect with ${MONGO_CONNECT}`);
