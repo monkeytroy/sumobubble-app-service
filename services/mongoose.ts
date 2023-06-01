d417ff1e462ee8e47738666a2a7cf9b67540dabb
Fred Arters <fredarters@gmail.com>
2023-06-01 02:03:25 -0400

    Fix dup db connections

--- services/mongoose.ts
+++ services/mongoose.ts
@@ -1,14 +1,18 @@
-import mongoose from 'mongoose';
+import mongoose, { mongo } from 'mongoose';
 
 const MONGO_CONNECT = process.env.MONGO_CONNECT;
 
+let mongoConnected = false
+
 const connectMongo = async () => {
   console.log(`Connecting with ${MONGO_CONNECT}`);
-  if (MONGO_CONNECT) {
-    let res = await mongoose.connect(MONGO_CONNECT);
+  if (MONGO_CONNECT && !mongoConnected) {
+    const res = await mongoose.connect(MONGO_CONNECT);
     if (res) {
+      mongoConnected = true;
       console.log(`Connected with ${MONGO_CONNECT}`);
     } else {
+      mongoConnected = false;
       console.log(`Failed to connect with ${MONGO_CONNECT}`);
     }
   } 
