e3e4adeec5c7054c944840eeb390ed70b9620f44
Fred Arters <fredarters@gmail.com>
2023-06-18 23:48:51 -0400

    Removed old code

--- services/mongoose.ts
+++ services/mongoose.ts
@@ -1,42 +1,29 @@
 import mongoose, { mongo } from 'mongoose';
+import { log } from './log';
 
 const MONGO_CONNECT = process.env.MONGO_CONNECT;
 
 let mongoConnected = false
-let onClose = false;
 
 const connectMongo = async () => {
 
   const ready = mongoose?.connection?.readyState;
   mongoConnected = (ready == 1 || ready == 2);
 
-  console.log(`connectMongo:: currently connected ${mongoConnected} 
+  log(`connectMongo:: currently connected ${mongoConnected} 
     readyState ${ready} connection count ${mongoose?.connections?.length}`);
   
   if (MONGO_CONNECT && !mongoConnected) {
-    console.log(`Connecting with ${MONGO_CONNECT}`);
+    log(`Connecting with ${MONGO_CONNECT}`);
     const res = await mongoose.connect(MONGO_CONNECT);
     if (res) {
       mongoConnected = true;
-      console.log(`Connected`, res.connection.readyState);
+      log(`Connected`, res.connection.readyState);
     } else {
       mongoConnected = false;
-      console.log(`Failed to connect with ${MONGO_CONNECT}`);
+      log(`Failed to connect with ${MONGO_CONNECT}`);
     }
   } 
-
-  if (!onClose) {
-    onClose = true;
-    process.on('beforeExit', () => {
-      console.log('SHUTDOWN / EXIT DETECTED... CLOSING DB CONNECTIONS');
-      disconnectMongo();
-    });
-    process.on('SIGTERM', () => {
-      console.log('SHUTDOWN / EXIT DETECTED... CLOSING DB CONNECTIONS');
-      disconnectMongo();
-    });
-  }
-
 }
 
 export const disconnectMongo = async () => {
